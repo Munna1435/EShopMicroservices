d09bc82696edb67d450ba9bb949cbff3da24a625
munna <kumarbadal1434@gmail.com>
2025-07-04 16:51:32 +0530

    cqrs and minimal api setup for catalog api

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,19 @@
+﻿using BuildingBlocks.CQRS;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+        : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+
+    public class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            // create Product entity from command object
+            // save to database
+            // return CreateProductResult result
+            return new CreateProductResult(Guid.NewGuid());
+        }
+    }
+}
