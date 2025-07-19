c7f4d4142b5d968fe97db522825f235c37dcf607
munna <kumarbadal1434@gmail.com>
2025-07-19 13:32:49 +0530

    Ordering.Domain layer is developed

---
+++ src/Services/Ordering/Ordering.Domain/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using Ordering.Domain.Abstractions;
+using Ordering.Domain.ValueObjects;
+
+namespace Ordering.Domain.Models
+{
+    public class Customer : Entity<CustomerId>
+    {
+        public string Name { get; private set; } = default!;
+        public string Email { get; private set; } = default!;
+
+        public static Customer Create(CustomerId id, string Name, string Email) {
+            ArgumentException.ThrowIfNullOrWhiteSpace(Name);
+            ArgumentException.ThrowIfNullOrWhiteSpace(Email);
+
+            return new Customer
+            {
+                Id = id,
+                Name = Name,
+                Email = Email
+            };
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Infrastructure
+{
+    public static class DepedencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            return services;
+        }
+    }
+}
